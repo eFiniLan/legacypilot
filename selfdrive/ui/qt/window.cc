035c15d623c66253b68f10bb69a236dc25f2cbab
Rick Lan <ricklan@gmail.com>
2023-04-25 16:10:47 +0800

    Fixed issue when wifi window was close automatically and cannot open again

--- selfdrive/ui/qt/window.cc
+++ selfdrive/ui/qt/window.cc
@@ -84,6 +84,15 @@ bool MainWindow::eventFilter(QObject *obj, QEvent *event) {
 
   if (evts.contains(event->type())) {
     device.resetInteractiveTimout();
+    #ifdef QCOM
+    // filter out touches while in android activity
+    if (HardwareEon::launched_activity) {
+      HardwareEon::check_activity();
+      if (HardwareEon::launched_activity) {
+        return true;
+      }
+    }
+    #endif
   }
   return false;
 }
