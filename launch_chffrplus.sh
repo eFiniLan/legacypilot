78d4df4961881dc7473e0c1563f4531b7716de5f
Rick Lan <ricklan@gmail.com>
2023-05-05 16:52:25 +0800

    Added default openpilot ssh key

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -10,6 +10,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 function two_init {
   mount -o remount,rw /system
+  # openpilot ssh key installer
+  if [ ! -f /data/params/d/GithubSshKeys ]; then
+    echo -n openpilot > /data/params/d/GithubUsername
+    cat /system/comma/home/setup_keys > /data/params/d/GithubSshKeys
+    echo -n 1 > /data/params/d/SshEnabled
+    setprop persist.neos.ssh 1
+  fi
   if [ ! -f /ONEPLUS ] && ! $(grep -q "letv" /proc/cmdline); then
     sed -i -e 's#/dev/input/event1#/dev/input/event2#g' ~/.bash_profile
     touch /ONEPLUS
