3defdacbf450e260b6caad29a58a559dbf41580e
Rick Lan <ricklan@gmail.com>
2023-05-12 15:52:03 +0800

    Use selfdrive.loggerd instead

--- selfdrive/athena/athenad.py
+++ selfdrive/athena/athenad.py
@@ -36,7 +36,7 @@ from common.file_helpers import CallbackReader
 from common.params import Params
 from common.realtime import sec_since_boot, set_core_affinity
 from system.hardware import HARDWARE, PC, AGNOS
-from system.loggerd.config import ROOT
+from selfdrive.loggerd.config import ROOT
 from selfdrive.loggerd.xattr_cache import getxattr, setxattr
 from selfdrive.statsd import STATS_DIR
 from system.swaglog import SWAGLOG_DIR, cloudlog
