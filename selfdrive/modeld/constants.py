9205b02c9eb1373d3b9952ebc91f493a88f49568
Rick Lan <ricklan@gmail.com>
2024-02-04 22:25:22 +0800

    Refactored into ModelConstants

--- selfdrive/modeld/constants.py
+++ selfdrive/modeld/constants.py
@@ -1,7 +1,8 @@
-IDX_N = 33
 
 def index_function(idx, max_val=192, max_idx=32):
   return (max_val) * ((idx/max_idx)**2)
 
-
-T_IDXS = [index_function(idx, max_val=10.0) for idx in range(IDX_N)]
+class ModelConstants:
+  # time and distance indices
+  IDX_N = 33
+  T_IDXS = [index_function(idx, max_val=10.0) for idx in range(IDX_N)]
